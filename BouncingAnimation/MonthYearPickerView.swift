8e3d8037130c2e8ac9e420d60b4a90881695c508
Amrit <tiwariammit@gmail.com>
2016-10-27 10:51:09 +0545

    file added

---
+++ BouncingAnimation/MonthYearPickerView.swift
@@ -0,0 +1,111 @@
+//
+//  MonthYearPicker.swift
+//
+//  Created by Ben Dodson on 15/04/2015.
+//
+
+import UIKit
+
+class MonthYearPickerView: UIView, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var datePicker: UIPickerView!
+    var resetBrightNess : (()->())?
+    
+    //    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let months : [Int] = [1, 2, 3, 4,5,6,7,8,9,10,11,12]
+    
+    var years: [Int] = []
+    
+    var selectedMnth : Int?
+    var selectedYear : Int?
+    var dateTranfer: (([String:Int?])->())?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonSetup()
+    }
+    
+    
+    
+    func commonSetup() {
+        
+        var yrs = NSCalendar(identifier: NSCalendarIdentifierGregorian)!.component(.Year, fromDate: NSDate())
+        
+        for _ in 1...6 {
+            
+            years.append(yrs)
+            yrs += 1
+        }
+        
+        
+        selectedMnth = 1
+        selectedYear = years[0]
+    }
+    
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return "\(months[row])"
+        case 1:
+            return "\(years[row])"
+        default:
+            return nil
+        }
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return months.count
+        case 1:
+            return years.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if component == 0{
+            
+            selectedMnth = months[row]
+        }
+        if component == 1{
+            
+            selectedYear = years[row]
+        }
+    }
+    
+    
+    @IBAction func btnAc(sender: AnyObject) {
+        
+        handleOnDismiss()
+    }
+    
+    
+    func handleOnDismiss(){
+        
+        let dic = ["year": self.selectedYear, "month": self.selectedMnth]
+        if let handler = self.dateTranfer{
+            
+            handler(dic)
+        }
+        if let handler = self.resetBrightNess{
+            
+            handler()
+        }
+        self.removeFromSuperview()
+        
+    }
+}
--- BouncingAnimation/ViewController.swift
+++ BouncingAnimation/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var backGroundView1 : UIView?
+
+    @IBOutlet weak var btnDate: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func PopOverPresent(sender: AnyObject) {
+        
+        
+        if let customView = NSBundle.mainBundle().loadNibNamed("AnnotationXibView", owner: self, options: nil)[0] as? MonthYearPickerView {
+            
+            
+            customView.frame = CGRect(x: 20, y: 150, width: self.view.frame.width - 40, height: self.view.frame.height - 300)
+            let backGroundViewSize = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            backGroundView1 = UIView(frame: backGroundViewSize)
+            
+            customView.resetBrightNess = {
+                
+                self.backGroundView1?.removeFromSuperview()
+            }
+            
+            customView.dateTranfer = {
+                date in
+                if let mnth = date["month"]{
+                    
+                    if let m = mnth{
+                        
+                        if let yrs = date["year"]{
+                            if let year = yrs{
+                                
+                                let str = "\(m)" + " / " + "\(year)"
+                                self.btnDate.setTitle(str, forState: UIControlState.Normal)
+                            }
+                        }
+                    }
+                }
+            }
+            if let v = backGroundView1{
+                
+                v.backgroundColor = UIColor.blackColor()
+                v.alpha = 0.7
+                self.view.addSubview(v)
+            }
+            
+            customView.transform = CGAffineTransformMakeScale(0, 0)
+            customView.layer.borderWidth = 5
+            customView.layer.borderColor = UIColor.redColor().CGColor
+            self.view.addSubview(customView)
+            UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 4, options: .CurveLinear, animations: {() -> Void in
+                
+                customView.transform = CGAffineTransformIdentity
+                
+                }, completion: { _ in })
+        }
+        
+
+    }
 
 }
 
